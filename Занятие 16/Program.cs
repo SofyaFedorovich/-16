4234bdd820300d1448f45cf1d9c36461f8644440
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-11 14:31:41 +0300

    Добавьте файлы проекта.

---
+++ Занятие 16/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Занятие_16
+{
+    class Program
+    {//разработать класс для моделирования объекта "Товар"
+        static void Main(string[] args)
+        {
+        }
+    }
+}
