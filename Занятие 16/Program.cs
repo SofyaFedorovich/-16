6c027f354a24416491f72d2a1892dad1d7307926
s.fedorovich <s.fedorovich@CZC71776GT>
2021-11-17 11:08:04 +0300

    Задача 1

--- Занятие 16/Program.cs
+++ Занятие 16/Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 
 namespace Занятие_16
 {
-    class Products //разработать класс для моделирования объекта "Товар"
+    public class Products //разработать класс для моделирования объекта "Товар"
     {
         int code;
         string name;
@@ -24,26 +24,29 @@ namespace Занятие_16
             this.name = name;
             this.price = price;
         }
+
+        
     }
     class Program
     {
         static async Task Main(string[] args)
         {
             Products products = new Products();
-            Console.WriteLine("Ввести код товара:");
+            Console.WriteLine("Ввести код товара:  ");
             products.Code = int.Parse(Console.ReadLine());
-            Console.WriteLine("Ввести название товара:");
+            Console.WriteLine("Ввести название товара:  ");
             products.Name = Convert.ToString(Console.ReadLine());
-            Console.WriteLine("Ввести цену товара:");
+            Console.WriteLine("Ввести цену товара:  ");
             products.Price = double.Parse(Console.ReadLine());
 
             const int n = 6;
-            int[] arrayCode = new int[6];
+            double[] arrayPrice = new double[6];
             for (int i = 1; i < n; i++)
             {
-                Console.WriteLine("Введите коды товаров {0}", i );
-                arrayCode[i] = int.Parse (Console.ReadLine());
-            }
+              
+                arrayPrice[i] = products.Price;
+                Console.Write("{0,5}", arrayPrice[i]);
+        }
             string jsonString = JsonSerializer.Serialize<Products>(products);
             Console.WriteLine(jsonString);
             using (FileStream fs = new FileStream("Products.json", FileMode.OpenOrCreate))
